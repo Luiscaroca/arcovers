1bbde7f447bec6b0ebaedae4055cd44f5e0c8061
Luis Joaquín Caroca Araya <lj.caroca@gmail.com>
2025-06-02 10:31:56 -0400

    agregado margin bottom a componente Como_comprar.vue

--- src/components/Como_comprar.vue
+++ src/components/Como_comprar.vue
@@ -1,6 +1,6 @@
 <template>
   <h1 class="text-center mt-3">Como comprar</h1>
-  <div class="col-md-6 col-sm-10 mx-auto">
+  <div class="col-md-6 col-sm-10 mx-auto mb-4">
     <ol class="list-group list-group-numbered fs-6">
       <li class="list-group-item">
         Contáctanos por tu vía preferida, comentándonos tus necesidades y el
